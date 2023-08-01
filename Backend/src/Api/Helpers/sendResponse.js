d17622bd384202eecc29a668e364e6eaec9b3134
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-07-31 22:07:03 -0500

    refactor (Helpers) : :recycle: Refactor Send
    Refactorizacion del mensaje en Helpers para cuando hay error

--- Backend/src/Api/Helpers/sendResponse.js
+++ Backend/src/Api/Helpers/sendResponse.js
@@ -1,8 +1,8 @@
 export function sendSuccessResponse(res, data, message, statusCode = 200) {
-  res.status(statusCode).json({ data, message });
+  res.status(statusCode).json({ message, data });
 }
 
 export function sendErrorResponse(res, err, statusCode = 500) {
   console.log(err);
-  res.status(statusCode).send({ error: "Error en el Server" });
+  res.status(statusCode).send({ error: "Error en el Server", message: err });
 }
